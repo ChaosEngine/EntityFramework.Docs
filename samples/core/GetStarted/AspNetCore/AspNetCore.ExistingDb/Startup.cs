a130898b762ab9069368207d6c904a906c95fba3
ChaosEngine <andrzej.pauli@gmail.com>
2017-01-17 11:53:55 +0100

    added sqlite additionally new EF migrations addition (using sqlite)

--- samples/core/GetStarted/AspNetCore/AspNetCore.ExistingDb/Startup.cs
+++ samples/core/GetStarted/AspNetCore/AspNetCore.ExistingDb/Startup.cs
@@ -53,11 +53,10 @@ namespace EFGetStarted.AspNetCore.ExistingDb
 			//options.UseSqlServer(configuration.GetConnectionString("SqlServer"));
 
 			//MySQL
-			options.UseMySQL(configuration.GetConnectionString("MySQL"));
+			//options.UseMySQL(configuration.GetConnectionString("MySQL"));
 
-			//
-			//TODO: SqlLite ??
-			//
+			//SqlLite
+			options.UseSqlite("Filename=./Blogging.db");
 		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
